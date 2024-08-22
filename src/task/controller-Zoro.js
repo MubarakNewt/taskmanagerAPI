12f4876ced67db963ba3b5fafd11ef793c01b122
Mubarak <mubaraknewt10@gmail.com>
2024-08-22 14:02:37 +0100

    update

---
+++ src/task/controller-Zoro.js
@@ -0,0 +1,260 @@
+import pool from "../../db.js"
+import queries from "./queries.js";
+
+const getUsers = (req, res) => {
+    pool.query(queries.getUsers, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id)
+    pool.query(queries.getUserById, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
+const getUserBoards = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getUserBoards, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
+const getColumnTask = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getColumnTask, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
+const getTasks = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getTasks, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
+const addUsers = (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getUserById, [id], (error, results) => {
+        const noUserFound = !results.rows.length;
+        if (noUserFound) {
+            res.send('no student found')
+        }
+    })
+
+    const {first_name, last_name, email} = req.body
+    // console.log('mememem', email)
+    pool.query(queries.addUsers, [first_name, last_name, email], (error, results) => {
+        if (error) throw error;
+        res.status(201).send('User added')
+    })
+
+}
+
+const addTasks =  (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getUserById, [id], (error, results) => {
+        const noUserFound = !results.rows.length;
+        if (noUserFound) {
+            res.send('no student found')
+        }
+    })
+
+    const {title, description, current_status, column_id} = req.body
+
+    pool.query(queries.addTasks, [title, description, current_status, column_id], (error, results) => {
+        if (error) throw error;
+        res.status(201).send('Task added')
+    })
+
+}
+
+const addSubTasks = (req, res) => {
+
+    // const id = parseInt(req.params.id)
+
+    // pool.query(queries.getUserById, [id], (error, results) => {
+    //     const noUserFound = !results.rows.length;
+    //     if (noUserFound) {
+    //         res.send('no student found')
+    //     }
+    // })
+
+    const {title, task_id} = req.body
+
+    pool.query(queries.addSubTasks, [title, task_id], (error, results) => {
+        if (error) throw error;
+        res.status(201).send('Task added')
+    })
+}
+
+const addColumn = (req, res) => {
+
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getUserById, [id], (error, results) => {
+        const noUserFound = !results.rows.length;
+        if (noUserFound) {
+            res.send('no student found')
+        }
+    })
+
+
+    const {name, colour_tag, task_count, board_id} = req.body
+
+    pool.query(queries.addColumn, [name, colour_tag, task_count, board_id], (error, results) => {
+        if (error) throw error;
+        res.status(201).send('Task added')
+    })
+}
+
+const addBoard = (req, res) => {
+    const {name, user_id} = req.body
+
+    pool.query(queries.addBoard, [title, task_id], (error, results) => {
+        if (error) throw error;
+        res.status(201).send('Task added')
+    })
+}
+
+const removeUser = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query(queries.getUserById, [id], (error, results) => {
+        const noUserFound = !results.rows.length;
+        if (noUserFound) {
+            res.send('no student found')
+        }
+    })
+
+    pool.query(queries.removeUser, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('User removed')
+    })
+
+}
+
+const removeBoard = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    // pool.query(queries.getUserById, [id], (error, results) => {
+    //     const noUserFound = !results.rows.length;
+    //     if (noUserFound) {
+    //         res.send('no student found')
+    //     }
+    // })
+
+    pool.query(queries.removeBoard, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('Board removed')
+    })
+
+}
+
+const removeTask = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    // pool.query(queries.getUserById, [id], (error, results) => {
+    //     const noUserFound = !results.rows.length;
+    //     if (noUserFound) {
+    //         res.send('no student found')
+    //     }
+    // })
+
+    pool.query(queries.removeTask, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('Task removed')
+    })
+
+}
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id)
+    const {first_name, last_name, email} = req.body
+    
+    pool.query(queries.getUserById, [id], (error, results) => {
+        const noUserFound = !results.rows.length;
+        if (noUserFound) {
+            res.send('no student found')
+        }
+    })
+
+    pool.query(queries.updateUser, [first_name, last_name, email, id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('User Updated')
+    })
+
+}
+
+const updateTask = (req, res) => {
+    const id = parseInt(req.params.id)
+    const {title, description, current_status, column_id} = req.body
+    
+    // pool.query(queries.getUserById, [id], (error, results) => {
+    //     const noUserFound = !results.rows.length;
+    //     if (noUserFound) {
+    //         res.send('no student found')
+    //     }
+    // })
+
+    pool.query(queries.updateTask, [title, description, current_status, column_id, id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('Task Updated')
+    })
+
+}
+
+const updateBoard = (req, res) => {
+    const id = parseInt(req.params.id)
+    const {name, user_id} = req.body
+    
+    // pool.query(queries.getUserById, [id], (error, results) => {
+    //     const noUserFound = !results.rows.length;
+    //     if (noUserFound) {
+    //         res.send('no student found')
+    //     }
+    // })
+
+    pool.query(queries.updateBoard, [name, user_id, id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send('Task Updated')
+    })
+
+}
+
+export default {
+    getUsers,
+    getUserById,
+    getUserBoards,
+    getColumnTask,
+    getTasks,
+
+    addUsers,
+    addTasks,
+    addSubTasks,
+    addBoard,
+    addColumn,
+
+    removeUser,
+    removeBoard,
+    removeTask,
+
+    updateUser,
+    updateTask,
+    updateBoard,
+}
+
---
+++ src/task/queries-Zoro.js
@@ -0,0 +1,56 @@
+
+const getUsers = 'SELECT * FROM users'
+const getUserById = 'SELECT * FROM users WHERE id = $1'
+const getUserBoards = 'SELECT * FROM boards JOIN users ON boards.user_id = users.id WHERE users.id = $1'
+const getColumnTask = 'SELECT boards.id, boards.name, columns.id, tasks.id, tasks.title, tasks.description, tasks.current_status FROM boards JOIN columns ON boards.id = columns.board_id JOIN tasks ON columns.id = tasks.column_id WHERE boards.id = $1'
+const getTasks = 'SELECT tasks.id, tasks.title, tasks.description, tasks.current_status, sub_tasks.id, sub_tasks.title, sub_tasks.is_completed FROM tasks JOIN sub_tasks ON tasks.id = sub_tasks.task_id WHERE tasks.id = $1'
+
+const addUsers = 'INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3)'
+const addTasks = 'INSERT INTO tasks (title, description, current_status, column_id) VALUES ($1, $2, $3, $4) RETURNING id'
+const addSubTasks = 'INSERT INTO sub_tasks (title, task_id) VALUES ($1, $2)'
+const addBoard ='INSERT INTO boards (name, user_id) VALUES ($1, $2)'
+const addColumn = 'INSERT INTO columns (name, colour_tag, task_count, board_id) VALUES ($1, $2, $3, $4)'
+// (title, task_id) VALUES ($1, $2)
+//  INSERT INTO tasks (title, description, due_date)
+//   VALUES (@title, COALESCE(@description, ''), COALESCE(@due_date, '1900-01-01'));
+
+
+const removeUser = 'DELETE FROM users WHERE id = $1'
+const removeBoard = 'DELETE FROM boards WHERE id = $1'
+const removeTask = 'DELETE FROM tasks WHERE id = $1'
+
+
+const updateUser  = 'UPDATE users SET first_name = COALESCE($1, first_name), last_name = COALESCE($2, last_name), email = COALESCE($3, email) WHERE id = $4'
+const updateTask = 'UPDATE tasks SET title = COALESCE($1, title), description = COALESCE($2, description), current_status = COALESCE($3, current_status), column_id = COALESCE($4, column_id) WHERE id = $5'
+// const updateSubTask = 'UPDATE sub_tasks SET title = COALESCE($1, title) WHERE id = $2'
+const updateBoard = 'UPDATE users SET name = COALESCE($1, name), user_id = COALESCE($2, user_id) WHERE id = $4'
+const updateColumn = ''
+
+export default {
+    getUsers,
+    getUserById,
+    getUserBoards,
+    getColumnTask,
+    getTasks,
+
+    addUsers,
+    addTasks,
+    addSubTasks,
+    addBoard,
+    addColumn,
+    
+    removeUser,
+    removeBoard,
+    removeTask,
+
+    updateUser,
+    updateTask,
+    updateBoard,
+}
+
+// GET /1/members/{memberID}/boards
+// -- Get all boards for the user with user_id 1
+// SELECT b.board_id, b.board_name, b.created_at
+// FROM boards b
+// JOIN users u ON b.user_id = u.user_id
+// WHERE u.user_id = 1;
